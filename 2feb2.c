1fabe08cd23ec780080b9e8c2b69fd8c0d729007
Kavya <kavyadangi09@gmail.com>
2023-02-23 02:24:57 +0530

    Create 2feb2.c

---
+++ 2feb2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct node{
+
+    int num;
+
+}n;
+struct node* get(int);
+void display(struct node*,int);
+int main(void){
+    struct node a[n];
+   
+    
+    
+    get();
+    display();
+    return 0;
+
+}
+struct node* get(int n){
+    struct a[n],*p;
+    p=a;
+    printf("enter %d elements")
+}
