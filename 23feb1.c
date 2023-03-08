bf679f7401b6cd61127852c8a2bbb640b083fb00
Kavya <kavyadangi09@gmail.com>
2023-03-08 15:20:43 +0530

    Create 23feb1.c

---
+++ 23feb1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int top;
+int main(){
+    int option;
+    do{
+        printf("\n\n MAIN MENU");
+        printf("\n1.Push function");
+        printf("\n2.Pop function");
+        printf("\n3.Display function");
+        printf("\n4.EXIT");
+        printf("enter option: ");
+        scanf("%d",&option);
+        switch(option){
+            case:1
+        }
+    }
+}
