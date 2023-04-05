71d5ab620ab18143f364f02b721022a0fa60a93d
Kavya <kavyadangi09@gmail.com>
2023-04-06 01:15:27 +0530

    Create grtstof3.c

---
+++ grtstof3.c
@@ -0,0 +1,41 @@
+#include<stdio.h> 
+int main(){ 
+    int a,b,c; 
+    printf("enter three numbers"); 
+    scanf("%d%d%d",&a,&b,&c); 
+    if(a>b){ 
+        if(a>c){ 
+            printf("a is the gretest"); 
+     
+        } 
+        else if(c>a){ 
+            printf(" c is the greatest"); 
+        } 
+         
+        else{ 
+        printf("Invalid"); 
+        } 
+     
+    } 
+    else if(b>a){ 
+       if(b>c){ 
+        printf("b is the greatest"); 
+ 
+       } 
+       else if (c>b){ 
+        printf("c is the greatest"); 
+       } 
+       else{ 
+        printf("Invalid"); 
+       } 
+         
+       } 
+    else if(a==b && a==c){
+        printf("all are equal");
+    }
+    else{ 
+        printf("Invalid"); 
+    } 
+     
+       return 0; 
+}
