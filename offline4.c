76c77a90494d9be334ff37b1c0c95db92ef1a01a
Kavya <kavyadangi09@gmail.com>
2023-04-09 00:40:11 +0530

    Create offline4.c

---
+++ offline4.c
@@ -0,0 +1,16 @@
+//wap func returning an array
+#include<stdio.h>
+#include<string.h>
+printarray(int arr[n]);
+int main(){
+    int n;
+    printf("enter no of values");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        scanf("%d"arr[i]);
+        
+        
+ 
+    }
+ 
+}
