d7ded1efcb5334f63ce5cee1f2f226b7e3ec2a5e
Kavya <kavyadangi09@gmail.com>
2023-04-10 14:37:12 +0530

    Create onlineclss1.c

---
+++ onlineclss1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char a[20];
+    char*b;
+    char s1[10]="hello";
+    char s2[]="tpday";
+    char s3[10]={'h','e','l','l','o'};
+    s1[0]='c';
+    printf("entered string: %s\n",s1);
+    return 0;
+    
+}
