d458b44ab6411a923ca286c015abccbb6c57d05b
Kavya <kavyadangi09@gmail.com>
2023-04-12 13:07:40 +0530

    Create quqe3mar.c

---
+++ quqe3mar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int enqueue(int)
+int dequeue(int)
+int rear=-1;
+int front=-1;
+int main(){
+   int n;
+   printf("enter the no of elemnts");
+   scanf("%d",&n);
+   int arr[n];
+   
+   
+    
+}
