2bdd2f9aeb7d6e7f34d26630a14d9f0ff93adaa3
Kavya <kavyadangi09@gmail.com>
2023-11-11 17:07:05 +0530

    Create fjf.c

---
+++ component 2.2/fjf.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<string.h>
+ struct student{
+
+
+
+int roll; union
+
+int g char school[4];
+ };
+
+int main()
+
+{
+
+struct student stud stud.roll 100)
+
+stud-age-65)
+
+printf(sollid, Aged, stul.roll, stud.age);
+
+strepy(sted school, "(SE")) 
+printf("mall schooliksi, stul.roll, stud, school);
+
+return 0;
+}
